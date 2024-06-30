ff24714fece99ab25917fdb5e81986605448cd96
Bimo Wahyu Syahputro <128451452+bimowahyu@users.noreply.github.com>
2024-07-01 01:18:30 +0700

    Update JamByIdController.js

--- backend/controllers/JamByIdController.js
+++ backend/controllers/JamByIdController.js
@@ -3,13 +3,35 @@ import Karyawan from '../models/Karyawan.js';
 import Jam from '../models/Jam.js';
 
 
+export const getJamByIdForKaryawan = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const jam = await JamById.findByPk(id);
+         
+        if(jam){
+          res.status(200).json(jam);
+        }else{
+          res.status(404).json({msg: "jam tidak ada"})
+        }
+        
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Internal Server Error" });
+        
+      }
+    };
+
 // export const getJamByIdForKaryawan = async (req, res) => {
 //     try {
 //         const { karyawanId } = req.params;
 
 //         const jamData = await JamById.findAll({
 //             where: { karyawan_id: karyawanId },
-//             include: [{ model: Jam, as: 'jamDetail' }] // Menggunakan alias 'jamDetail' yang telah ditentukan di model
+//             include: [{ 
+//                 model: Jam, 
+//                 as: 'jamDetail', // Pastikan alias 'jamDetail' sudah didefinisikan
+//                 attributes: ['id', 'name', 'hari', /* tambahkan kolom 'hari' di sini */] // Termasuk kolom "hari" di sini
+//             }]
 //         });
 
 //         if (!jamData || jamData.length === 0) {
@@ -22,30 +44,6 @@ import Jam from '../models/Jam.js';
 //         res.status(500).json({ msg: "Internal server error" });
 //     }
 // };
-// 
-export const getJamByIdForKaryawan = async (req, res) => {
-    try {
-        const { karyawanId } = req.params;
-
-        const jamData = await JamById.findAll({
-            where: { karyawan_id: karyawanId },
-            include: [{ 
-                model: Jam, 
-                as: 'jamDetail', // Pastikan alias 'jamDetail' sudah didefinisikan
-                attributes: ['id', 'name', 'hari', /* tambahkan kolom 'hari' di sini */] // Termasuk kolom "hari" di sini
-            }]
-        });
-
-        if (!jamData || jamData.length === 0) {
-            return res.status(404).json({ msg: "Data jam tidak ditemukan" });
-        }
-
-        res.status(200).json({ data: jamData });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ msg: "Internal server error" });
-    }
-};
 
 
 export const setJamById = async (req, res) => {
