bca1631ed11aa2f2f78fb2010193e4e5a7013de8
Bimo Wahyu Syahputro <128451452+bimowahyu@users.noreply.github.com>
2024-07-24 01:19:47 +0700

    realtime data swr

--- frontend/src/component/admin/DataAbsenHarian.jsx
+++ frontend/src/component/admin/DataAbsenHarian.jsx
@@ -1,36 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
+import useSWR from 'swr';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
+const fetcher = (url) => axios.get(url).then(res => res.data);
+
 const AbsensiChart = () => {
-  const [absensiData, setAbsensiData] = useState([]);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Mendapatkan bulan dan tahun saat ini
-        const now = new Date();
-        const bulan = now.getMonth() + 1; // Perhatikan bahwa bulan dimulai dari 0, jadi perlu ditambah 1
-        const tahun = now.getFullYear();
+  const now = new Date();
+  const bulan = now.getMonth() + 1;
+  const tahun = now.getFullYear();
 
-        // Mengirimkan permintaan ke backend dengan bulan dan tahun saat ini
-        const response = await axios.get(`http://localhost:5000/absensitotal/get?bulan=${bulan}&tahun=${tahun}`);
-        const data = response.data;
+  const { data, error } = useSWR(`http://localhost:5000/absensitotal/get?bulan=${bulan}&tahun=${tahun}`, fetcher);
 
-        // Mengonversi data absensi menjadi format yang diperlukan oleh Recharts
-        const chartData = Object.keys(data).map(karyawanId => ({
-          name: `Karyawan ${karyawanId}`,
-          kehadiran: data[karyawanId]
-        }));
+  if (error) return <div>Error loading data</div>;
+  if (!data) return <div>Loading...</div>;
 
-        setAbsensiData(chartData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+ 
+  const chartData = Object.keys(data).map(karyawanId => ({
+    name: `Karyawan ${karyawanId}`,
+    kehadiran: data[karyawanId]
+  }));
 
   return (
     <div>
@@ -39,7 +28,7 @@ const AbsensiChart = () => {
         <LineChart
           width={600}
           height={400}
-          data={absensiData}
+          data={chartData}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         >
           <XAxis dataKey="name" />
