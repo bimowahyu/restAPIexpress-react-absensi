c87a931f57fff4806f48afb0d5fb48751bec2c59
Bimo Wahyu Syahputro <128451452+bimowahyu@users.noreply.github.com>
2024-08-19 01:59:56 +0700

    Update lokasi.jsx

--- frontend/src/component/lokasi.jsx
+++ frontend/src/component/lokasi.jsx
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
 import L from 'leaflet'; 
-import axios from 'axios'; // Tambahkan axios untuk mengambil data
+import axios from 'axios';
 import markerIcon from './marker2.png'; 
 
 const Lokasi = ({ latitude, longitude }) => {
-  const [radius, setRadius] = useState(null); // Tambahkan state untuk radius
+  const [branchData, setBranchData] = useState(null);
 
   useEffect(() => {
     const fetchBranchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/cabang'); // Ganti URL dengan endpoint yang sesuai
-        const { radius } = response.data; // Asumsikan bahwa nilai radius ada di dalam data cabang yang diterima
-        setRadius(radius);
+        const response = await axios.get('http://localhost:5000/cabang');
+        const data = response.data.find(branch => branch.nama_cabang === "testcabang10km"); 
+        setBranchData(data);
       } catch (error) {
         console.error('Error fetching branch data:', error);
       }
@@ -22,9 +22,12 @@ const Lokasi = ({ latitude, longitude }) => {
     fetchBranchData();
   }, []);
 
-  if (latitude === null || longitude === null) {
+  if (!branchData || latitude === null || longitude === null) {
     return null;
   }
+
+  const [lang_kantor, long_kantor] = branchData.lokasi_kantor.split(',');
+  const radius = branchData.radius;
 
   const customMarkerIcon = L.icon({
     iconUrl: markerIcon,
@@ -34,7 +37,7 @@ const Lokasi = ({ latitude, longitude }) => {
   });
 
   return (
-    <MapContainer center={[latitude, longitude]} zoom={17} style={{ height: '300px', width: '300px' }}>
+    <MapContainer center={[latitude, longitude]} zoom={17} style={{ height: '300px', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -42,15 +45,13 @@ const Lokasi = ({ latitude, longitude }) => {
       <Marker position={[latitude, longitude]} icon={customMarkerIcon}>
         <Popup>Your Location</Popup>
       </Marker>
-      {radius && (
-        <Circle
-          center={[latitude, longitude]}
-          radius={radius}
-          color='blue'
-          fillColor='#f03'
-          fillOpacity={0.5}
-        />
-      )}
+      <Circle
+        center={[lang_kantor, long_kantor]}
+        radius={radius}
+        color='red'
+        fillColor='#f03'
+        fillOpacity={0.5}
+      />
     </MapContainer>
   );
 };
